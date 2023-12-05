2e086c3969f23ae59c9bd80b9647c6c265589259
Daiana Michels <daianaem.12@gmail.com>
2023-12-05 11:01:05 -0300

    created first component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: { Banner }
+}
+</script>
+
 <template>
-  <h1>Hello World! Vue :)</h1>
+  <Banner />
 </template>
