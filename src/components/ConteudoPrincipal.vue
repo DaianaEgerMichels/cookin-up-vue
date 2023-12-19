a346a7264b78cd29eae18066f62943a83851c743
Daiana Michels <daianaem.12@gmail.com>
2023-12-19 20:17:34 -0300

    added more components

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import Tag from './Tag.vue';
 
 export default {
   data() {
@@ -7,7 +8,7 @@ export default {
       ingredientes: ['Alho', 'Manteiga', 'Orégano']
     };
   },
-  components: { SelecionarIngredientes }
+  components: { SelecionarIngredientes, Tag }
 }
 </script>
 
@@ -19,8 +20,8 @@ export default {
       </span>
 
       <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
-          {{ ingrediente }}
+        <li v-for="ingrediente in ingredientes" :key="ingrediente">
+          <Tag :texto="ingrediente" ativa />
         </li>
       </ul>
 
@@ -33,6 +34,7 @@ export default {
     <SelecionarIngredientes />
   </main>
 </template>
+
 
 <style scoped>
 .conteudo-principal {
@@ -61,17 +63,6 @@ export default {
   flex-wrap: wrap;
 }
 
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-  transition: 0.2s;
-  color: var(--creme, #FFFAF3);
-  background: var(--coral, #F0633C);
-  font-weight: 700;
-}
 
 .lista-vazia {
   display: flex;
---
+++ src/components/SuaLista.vue
@@ -0,0 +1,53 @@
+<script lang="ts">
+import type { PropType } from 'vue';
+import Tag from './Tag.vue';
+export default {
+  components: { Tag },
+  props: {
+    ingredientes: { type: Array as PropType<string[]>, required: true }
+  },
+}
+</script>
+
+<template>
+  <section>
+    <span class="subtitulo-lg sua-lista-texto">
+      Sua lista:
+    </span>
+
+    <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+      <li v-for="ingrediente in ingredientes" :key="ingrediente">
+        <Tag :texto="ingrediente" ativa />
+      </li>
+    </ul>
+
+    <p v-else class="paragrafo lista-vazia">
+      <img src="../assets/imagens/icones/lista-vazia.svg" alt="Ícone de pesquisa">
+      Sua lista está vazia, selecione ingredientes para iniciar.
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.sua-lista-texto {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+</style>
